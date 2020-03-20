50f3efce88116c4eed6f4c0a0e6373439ca0c684
CharanNK <charkeet@gmail.com>
2020-03-20 06:05:49 +0530

    Initial Module add

---
+++ springcookbook/src/main/java/com/charan/springcookbook/SpringcookbookApplication.java
@@ -0,0 +1,13 @@
+package com.charan.springcookbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringcookbookApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringcookbookApplication.class, args);
+    }
+
+}
---
+++ springcookbook/src/test/java/com/charan/springcookbook/SpringcookbookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.charan.springcookbook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringcookbookApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
