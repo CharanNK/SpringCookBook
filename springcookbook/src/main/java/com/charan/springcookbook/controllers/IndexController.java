1abcd53bf5d5fa3b994b24df4f3925aeecd38231
CharanNK <charkeet@gmail.com>
2020-03-26 18:37:43 +0530

    one to one between notes and recipe. many to one between ingredients and recipe

---
+++ springcookbook/src/main/java/com/charan/springcookbook/controllers/IndexController.java
@@ -0,0 +1,13 @@
+package com.charan.springcookbook.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+
+    @RequestMapping({"","/","index.html","/index"})
+    public String getIndexPage(){
+        return "index";
+    }
+}
