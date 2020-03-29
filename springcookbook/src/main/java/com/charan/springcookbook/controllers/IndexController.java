dfb28c5f441aa6d9a2376499eda1bb437b836cad
CharanNK <charkeet@gmail.com>
2020-03-29 23:58:28 +0530

    Displaying the list of recipes on index page

--- springcookbook/src/main/java/com/charan/springcookbook/controllers/IndexController.java
+++ springcookbook/src/main/java/com/charan/springcookbook/controllers/IndexController.java
@@ -1,13 +1,30 @@
 package com.charan.springcookbook.controllers;
 
+import com.charan.springcookbook.domain.Category;
+import com.charan.springcookbook.domain.UnitOfMeasure;
+import com.charan.springcookbook.repositories.CategoryRepository;
+import com.charan.springcookbook.repositories.UnitOfMeasureRepository;
+import com.charan.springcookbook.services.RecipeService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Optional;
 
 @Controller
 public class IndexController {
 
+    private final RecipeService recipeService;
+
+    public IndexController(RecipeService recipeService) {
+        this.recipeService = recipeService;
+    }
+
     @RequestMapping({"","/","index.html","/index"})
-    public String getIndexPage(){
+    public String getIndexPage(Model model){
+
+        model.addAttribute("recipes",recipeService.getRecipes());
+
         return "index";
     }
 }
